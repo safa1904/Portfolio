c3c1c59fcd0fa43a44061e06439949569d722e9c
Saphia Mohamed <100108435+safa1904@users.noreply.github.com>
2024-01-23 18:54:51 -0700

    Rotation,Brid&Plane 55:13

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
-import { Sky } from '../models/Sky'
 import Island from '../models/Island'
+import Sky from '../models/Sky'
+import Bird from '../models/Bird'
+import Plane from '../models/Plane'
+
 
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex item-center justify-center">
@@ -30,12 +33,14 @@ const Home = () => {
           <ambientLight intensity={0.5}/>
           <hemisphereLight skyColor = "#b1e1ff" groundColor="#000000"
           intensity={1}/>
+          <Bird/>
           <Sky/>
           <Island
           position= {islandPosition}
           scale= {islandScale}
           rotation= {islandRotation}
           />
+          <Plane/>
         </Suspense>
       </Canvas>
     </section>
