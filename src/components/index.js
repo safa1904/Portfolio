1376012521447d4cf682c5928bcd2a2d9b37a5a4
Saphia Mohamed <100108435+safa1904@users.noreply.github.com>
2024-02-10 14:28:16 -0700

    redo projects and about

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,10 @@ import Loader from "./Loader";
 import Navbar from "./Navbar";
 import Alert from "./Alert";
 import HomeInfo from "./HomeInfo";
+import CTA from "./CTA";
 
 export {
-   
+   CTA,
     Loader,
     Navbar,
    Alert, 
